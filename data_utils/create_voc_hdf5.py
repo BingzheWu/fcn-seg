1f5a08f8b74952f97935247f4036541a15f29610
bingzhe <1200010666@pku.edu.cn>
2016-09-25 22:58:59 +0800

    add code for creating hdf5 of voc

--- data_utils/create_voc_hdf5.py
+++ data_utils/create_voc_hdf5.py
@@ -3,6 +3,36 @@ This is a script for creating hdf5 files of VOC segmentation train and
 val data set.
 author: Bingzhe Wu
 '''
-import hdf5
-voc_dir = '~/VOC/VOC2012/'#This is your VOC dir
+import h5py
+import copy
+import os
+import numpy as np
+from PIL import Image
+from voc_helper import voc
+voc_dir = '~/bingzhe/data/VOC2012/'#voc_dir is /path/to/VOC2012
+voc_ = voc(voc_dir)
+
+def create_hdf5(mode, save_path):
+    '''
+    if mode is 'image', this code will generate the image data.
+    Otherwise, the label data will be generated.
+    '''
+    images = []
+    image_list = os.listdir(voc_.dir)
+    for image in image_list:
+        if mode == 'image':
+            tmp = voc_.load_image(image.split('jpg')[0])
+        else:
+            tmp = voc_.load_label(image.split('jpg')[0])
+        images.append(tmp)
+    images = np.array(images)
+    h5f = h5py.File(save_path+'voc_'+mode, 'w')
+    h5f.create_dataset('voc_'+mode, data = images) 
+if __name__ == '__main__':
+    save_path = '~/bingzhe/data/voc_hdf5/'
+    mode = 'image'
+    create_hdf5(mode, save_path)
+
+
+
 
